50965d48d98a809e5779e4fc2ace584a56da4af0
rygdsddssd <2225194264@qq.com>
2024-08-26 16:25:14 +0800

    maptools-item的id

--- webgis/simple-webgis/src/components/MapTools.vue
+++ webgis/simple-webgis/src/components/MapTools.vue
@@ -1,35 +1,40 @@
 <template>
-    <div class="maptools-view">
-      <span class="maptools-item">行政区划导航</span>
-      <span class="maptools-item">距离测量</span>
-      <span class="maptools-item">面积测量</span>
-      <span class="maptools-item">清屏</span>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'MapTools',
-  };
-  </script>
-  
-  <style>
-  .maptools-view {
-    position: absolute;
-    padding: 0 15px;
-    height: 30px;
-    top: 20px;
-    right: 15px;
-    background-color: aliceblue;
-  }
-  .maptools-item {
-    line-height: 30px;
-    margin-left: 15px;
-    color: black;
-    cursor: pointer;
-  }
-  .maptools-item:first-child {
-    margin-left: 0;
-  }
-  </style>
-  
+  <div class="maptools-view">
+    <span class="maptools-item" @click="handleMapToolsItemClick" id="xzqh">行政区划导航</span>
+    <span class="maptools-item" @click="handleMapToolsItemClick" id="maptree">目录树管理</span>
+    <span class="maptools-item" @click="handleMapToolsItemClick" id="distance">距离测量</span>
+    <span class="maptools-item" @click="handleMapToolsItemClick" id="area">面积测量</span>
+    <span class="maptools-item" @click="handleMapToolsItemClick" id="clear">清屏</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapTools',
+  methods: {
+    handleMapToolsItemClick(e) {
+      console.log(e.target.id);
+    },
+},
+};
+</script>
+
+<style>
+.maptools-view {
+  position: absolute;
+  padding: 0 15px;
+  height: 30px;
+  top: 20px;
+  right: 15px;
+  background-color: aliceblue;
+}
+.maptools-item {
+  line-height: 30px;
+  margin-left: 15px;
+  color: black;
+  cursor: pointer;
+}
+.maptools-item:first-child {
+  margin-left: 0;
+}
+</style>
