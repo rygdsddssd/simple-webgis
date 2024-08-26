a095526bae6557e00ad2e2fde0dc9b58ee515414
rygdsddssd <2225194264@qq.com>
2024-08-26 14:21:11 +0800

    修复了ruter.push不能正确跳转

--- webgis/simple-webgis/router/router.js
+++ webgis/simple-webgis/router/router.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import DataVisval from './../pages/DataVisual.vue'
+import DataVisval from './../pages/DataVisual.vue';
 import OneMap from './../pages/OneMap.vue';
-
 Vue.use(VueRouter);
+const originalPush = VueRouter.prototype.push
+// 修改 原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
 
 export default new VueRouter({
     routes: [
